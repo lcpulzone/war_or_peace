3cdf97ccba395c44601142b903b048dba3575908
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-03-20 12:02:15 -0700

    Create methods for basic and war

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,4 +8,31 @@ class Turn
     @player2 = player2
     @spoils_of_war = []
   end
+
+  def type
+    if basic
+      :basic
+    elsif war
+      :war
+    end
+  end
+
+  def basic
+    if player1.rank_of_card(0) > player2.rank_of_card(0)
+      spoils_of_war << player2.deck.cards[0]
+      :basic
+    elsif
+      player1.rank_of_card(0) < player2.rank_of_card(0)
+      spoils_of_war << player1.deck.cards[0]
+      :basic
+    end
+  end
+
+  def war
+    player1.rank_of_card(0) == player2.rank_of_card(0)
+    spoils_of_war << player1.deck.cards[0..2]
+    spoils_of_war << player2.deck.cards[0..2]
+    spoils_of_war.flatten!
+    :war
+  end
 end
