2da17c8a9abbb0408632e62124c02b5104efd25c
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-03-20 12:01:30 -0700

    Create tests for basic and war

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -42,4 +42,30 @@ class TurnTest < Minitest::Test
     assert_equal @player2, @turn.player2
     assert_equal [], @turn.spoils_of_war
   end
+
+  def test_it_can_have_a_basic_turn
+    assert_equal :basic, @turn. type
+    assert_equal [@card3], @turn.spoils_of_war
+  end
+
+  def test_it_can_have_a_war_turn
+    @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    @player1 = Player.new("Megan", @deck1)
+    @deck1.add_card(@card1)
+    @deck1.add_card(@card2)
+    @deck1.add_card(@card5)
+    @deck1.add_card(@card8)
+
+    @deck2 = Deck.new([@card4, @card3, @card6, @card7])
+    @player2 = Player.new("Aurora", @deck2)
+    @deck2.add_card(@card4)
+    @deck2.add_card(@card3)
+    @deck2.add_card(@card6)
+    @deck2.add_card(@card7)
+
+    @turn = Turn.new(@player1, @player2)
+
+    assert_equal :war, @turn.type
+    assert_equal [@card1, @card2, @card5, @card4, @card3, @card6], @turn.spoils_of_war
+  end
 end
